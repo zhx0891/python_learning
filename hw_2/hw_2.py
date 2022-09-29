683d8e54aa3f92c1a80f914a96b1805a9f045fa4
zhukovv <northbug@yandex.ru>
2022-09-29 15:49:56 +0500

    fourth task solved

--- hw_2/hw_2.py
+++ hw_2/hw_2.py
@@ -1,3 +1,4 @@
+from random import randint
 # def sum_of_digit(n):
 #  Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N
 def second_task(N):
@@ -7,6 +8,8 @@ def second_task(N):
         tmp = tmp * i
         sequ.append(tmp)
     return sequ
+
+# Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму, округлённую до трёх знаков после точки.
 
 
 def third_task(n):
@@ -18,9 +21,21 @@ def third_task(n):
     return round(ind, 3)
 
 
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на позициях a и b. Значения N, a и b вводит пользователь с клавиатуры.
 
 
+def fourth_task(N, a, b):
+    if b == N: b -= 1
+    sequence = []
+    for i in range(N):
+        sequence.append(randint(-N, N))
+
+    return sequence[a] * sequence[b]
 
 
 # print(second_task(4))
-print(third_task(6))
+# print(third_task(6))
+# N = int(input('N?'))
+# a = int(input('a?'))
+# b = int(input('b?'))
+# print(fourth_task(N, a, b))
