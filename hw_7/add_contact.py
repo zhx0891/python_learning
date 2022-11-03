fcce5016fa46346f0095b8683d870fe766ff6ef5
zhukovv <northbug@yandex.ru>
2022-11-03 16:01:50 +0500

    добавлен модуль добавить контакт

--- hw_7/add_contact.py
+++ hw_7/add_contact.py
@@ -1,2 +1,18 @@
+
 def doit():
-    print('added')
+    print('Добавляю контакт\n')
+    name = input(' Имя Фамилия: \n')
+    phone = input(' Номер телефона: \n')
+    comment = input(' Комментарий: \n')
+    with open('phonebook', 'a', encoding='utf-8') as book:
+        book.write(f'{name} \n')
+        book.write(f'{phone} \n')
+        book.write(f'{comment} \n')
+        book.write(f'\n')
+
+
+print('Контакт добавлен.')
+
+
+
+
