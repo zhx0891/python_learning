0b00871ff7d52193370de5aa74311f6bc3d0e4cc
zhukovv <northbug@yandex.ru>
2022-11-04 15:51:54 +0500

    доработан экспорт хтмл

--- hw_7/exp_html.py
+++ hw_7/exp_html.py
@@ -1,8 +1,35 @@
-def do_html(file):
-	import menu
-	with open('html_book.html', 'w', encoding='utf-8') as book:
-		book.write('style ="font-size: 24px;"')
-		book.write('<html>\n<head></head>\n<body>\n')
-		book.write(f'<p> {file} </p>')
-		book.write('</body>\n</html>')
+def do_html():
+	with open('pattern.html', 'r', encoding='utf-8') as patt:
+		buff = patt.read()
+	data = []
+	with open('phonebook', 'r', encoding='utf-8') as book:
+		data = book.read()
 
+	data = data.split('\n\n')
+	data.pop()
+
+
+	with open('html_book.html', 'w', encoding='utf-8') as exp:
+		exp.write(buff)
+		for i in range(len(data)):
+			e = (data[i].split(' \n'))
+			exp.write(f'\n <strong>{e[0]}</strong><br>')
+			exp.write(f'\n <font color ="red">{e[1]}</font><br>')
+			exp.write(f'\n <i>{e[2]}</i><br><br>')
+
+		exp.write('</body>')
+		exp.write('</html>')
+
+
+
+
+
+
+
+
+
+
+
+
+
+do_html()
