3af514bf483f18969dbfae329d3af27f820eadfa
zhukovv <northbug@yandex.ru>
2022-11-24 11:48:19 +0500

    реализован фукционал простого калькулятора

--- hw_9/pytelebotapi_exp.py
+++ hw_9/pytelebotapi_exp.py
@@ -4,25 +4,53 @@ bot = TeleBot('5498732265:AAHE1PuQe_8EyDLBpxnSAIpHzCHoErOIoFU')
 
 @bot.message_handler(commands= ['help'])
 def help(message):
-    msg = bot.send_message(message.chat.id, " да или нет? ")
-    bot.register_next_step_handler(msg, user_answer)
+    msg = bot.send_message(message.chat.id, " /calc - калькулятор")
+    msg = bot.send_message(message.chat.id, " /wtf - генератор мата\n/phone телефонный справочник")
+    # bot.register_next_step_handler(msg, user_answer)
+
+@bot.message_handler(commands= ['calc'])
+def calc(message):
+    msg = bot.send_message(message.chat.id, "введите выражение типа (число) (действие) (число) "
+                                            "разделяя вводимые значения и действие пробелом\n"
+                                            "доступны действия + - * \\ ")
+    bot.register_next_step_handler(msg, calc)
+def calc(message):
+    actions = ('+', '-', '*', '/')
+    mess = message.text
+    spl_mess = mess.split(' ')
+    if spl_mess[0].isdigit() and spl_mess[2].isdigit():
+        a = int(spl_mess[0])
+        b = int(spl_mess[2])
+        action = spl_mess[1]
+        if action == '+':
+            bot.send_message(message.chat.id, f"{a} + {b} = {a + b}")
+        elif action == '-':
+            bot.send_message(message.chat.id, f"{a} - {b} = {a - b}")
+        elif action == '*':
+            bot.send_message(message.chat.id, f"{a} * {b} = {a * b}")
+        elif action == '/':
+            bot.send_message(message.chat.id, f"{a} / {b} = {a / b}")
+        else:
+            msg = bot.send_message(message.chat.id, "введено некорректное/недоступное действие")
 
 
-def user_answer(message):
-    if message.text == "да":
-        msg = bot.send_message(message.chat.id, " Что у тебя есть? ")
-        bot.register_next_step_handler(msg, user_reg)
-    elif message == "нет":
-        bot.send_message(message.chat.id, "нет? ")
     else:
-        bot.send_message(message.chat.id, " Што?) ")
+        bot.send_message(message.chat.id, "Введены некорректные данные ")
+    msg = bot.send_message(message.chat.id, "Продолжим?")
+
+
+
+    # if message.text == "":
+    #     msg = bot.send_message(message.chat.id, " Что у тебя есть? ")
+    #     bot.register_next_step_handler(msg, user_reg)
+    # elif message == "нет":
+    #     bot.send_message(message.chat.id, "нет? ")
+    # else:
+    #     bot.send_message(message.chat.id, " Што?) ")
 
 
 def user_reg(message):
     bot.send_message(message.chat.id, f" Только {message.text} ??? ")
-
-
-
 
 
 
--- testr.py
+++ testr.py
@@ -1,28 +1,24 @@
-# n = int(input())
-#
-# lst = list()
-# tmp = list()
-#
-# for i in range(n):
-# 	lst.append(tmp)
-# 	for j in range(n):
-# 		tmp.append(j)
-# 	tmp = tmp.clear()
-# 	print(lst)
-# 	print('\n')
-
-n = 8
-lst = list()
-tmp = list([i for i in range(n)] for i in range(n))
+def printArr(arr):
+	for i in arr:
+		print()
+		for j in i:
+			print(j, end=' ')
 
 
+n = 10
+lst = list([i for i in range(n)] for i in range(n))
+for i in range(len(lst)):
+	val = i
+	if i > 0:
+		act = '-'
+	for j in range(len(lst)):
+		if val == 0:
+			act = '+'
+		if act == '+':
+			lst[j][i] = val
+			val += 1
+		else:
+			lst[j][i] = val
+			val -= 1
+printArr(lst)
 
-
-
-
-#
-# for i in range(n):
-# 	for j in i:
-# 		print(j)
-# 	print()
-# print(lst)
